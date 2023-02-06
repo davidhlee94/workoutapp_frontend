f2d527be9a7506579aba2c119c84f29ed413cbba
davidhlee94 <davidhlee94@gmail.com>
2023-02-06 09:03:18 -0800

    changed about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,13 +8,13 @@ const About = () => {
             <div className="about-container">
                 <h1 className="about-header">About</h1>
                 <h4>Welcome to Gainzville</h4>
-                <p>Gainzville is a comprehensive web application designed to streamline your workout planning and execution. With an emphasis on simplicity and usability, Gainzville helps you plan your workouts, learn about the major muscle groups and their optimal engagement techniques.</p>
+                <p>Gainzville is a comprehensive web application designed to streamline your workout planning and execution. With an emphasis on simplicity, Gainzville helps you plan your workouts, learn about the major muscle groups and their optimal engagement techniques.</p>
 
                 <p>This app was created in response to the frustration of using multiple workout tracking tools with confusing interfaces and often, unnecessary fees. At Gainzville, we believe that working out should be simple and straightforward, without wasting time on complex interfaces. With this in mind, we have built a web app that is easy to navigate and understand, with the ultimate goal of helping you plan your gym sessions and execute them efficiently.
                 </p>
 
                 <p>Cheers to achieving your workout goals!</p>
-                <p>-David</p>
+                <p>-Gainzville Team</p>
             </div>
         </div>
     )
